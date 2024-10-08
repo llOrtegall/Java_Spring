91e6898a198530e2f46982c274a1f7eaa451f884
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-10-07 22:55:28 -0500

    Refactor CustomerController to add new endpoints for test2 and test3

--- src/main/java/ortega/clients/controllers/CustomerController.java
+++ src/main/java/ortega/clients/controllers/CustomerController.java
@@ -1,63 +1,62 @@
 package ortega.clients.controllers;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ortega.clients.entities.Customer;
 
-@RestController // esto es una anotación
+@RestController
 public class CustomerController {
 
-  @GetMapping("/test")
-  public String test(){
-    return "Hello World";
+  @GetMapping("/customer/1") // Obtener cliente por id
+  public Customer getCustomer(){
+
+    Customer customer = new Customer();
+    customer.setId(1);
+    customer.setFirstName("Juan");
+    customer.setLastName("Perez");
+    customer.setEmail("test@gmail.com");
+    customer.setDocument(1118335235);
+
+    return customer;
   }
 
-  @GetMapping("/test2")
-  public List<String> test2(){
-    List<String> list = new ArrayList<>();
-    list.add("Ivan");
-    list.add("Ortega");
-    list.add("Gonzalez");
-    list.add("Developer");
+  /*
+  @GetMapping("/customers") // Listar clientes
+  public List<Customer> getAllCustomers(){
+    return null;
+  }
+
+  @GetMapping("/customers") // Agregar cliente
+  public void addCustomer(Customer customer){
+    
+  }
+
+  @GetMapping("/customers") // Eliminar cliente
+  public void removeCliente(){
+    
+  }
+
+  @GetMapping("/customers") // Actualizar cliente
+  public void updateCliente(){
+    
+  }
+
+  @GetMapping("/test")
+  public List<Customer> searchCliente(){
+    List<Customer> list = new ArrayList<>();
 
     return list;
   }
 
-  @GetMapping("/test3")
-  public Map<String, String> test3(){
-    Map<String, String> map = new HashMap<>();
-    map.put("name", "Ivan");
-    map.put("lastname", "Ortega");
-    map.put("nickname", "IvanOrtega");
-    map.put("email", "Ivan_97@hotmail.com");
-
-    return map;
-  }
-
-  @GetMapping("/test4")  
-  public Customer test4(){
-    Customer c = new Customer();
-    c.setFirstName("Ivan");
-    c.setLastName("Ortega");
-    c.setEmail("test@gmail.com");
-    c.setAddress("Calle falsa 123");
-
-    return c;
-  }
+   */
 
   @GetMapping("/test5")
   public List<Customer> test5(){
     List<Customer> list = new ArrayList<>();
-    list.add(new Customer("Ivan", "Ortega", "test1@gmail.com", "Calle falsa 123"));
-    list.add(new Customer("Juan", "Perez", "test2@gmail.com", "Calle falsa 456"));
-    list.add(new Customer("Pedro", "Lopez", "test4@gmail.com", "Calle falsa 789"));
-    list.add(new Customer("Maria", "Gonzalez", "test5@gmail.com", "Calle falsa 101112"));
 
     return list;
   }
--- src/main/java/ortega/clients/entities/Customer.java
+++ src/main/java/ortega/clients/entities/Customer.java
@@ -1,43 +1,16 @@
 package ortega.clients.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+  private Integer id;
   private String firstName;
   private String lastName;
   private String email;
-  private String address;
-
-  public Customer() {
-  }
-
-  public Customer(String firstName, String lastName, String email, String address) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.address = address;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-  public String getLastName() {
-    return lastName;
-  }
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-  public String getEmail() {
-    return email;
-  }
-  public void setEmail(String email) {
-    this.email = email;
-  }
-  public String getAddress() {
-    return address;
-  }
-  public void setAddress(String address) {
-    this.address = address;
-  }  
+  private Integer document;
 }
