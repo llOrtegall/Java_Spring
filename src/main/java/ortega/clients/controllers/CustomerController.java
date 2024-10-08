63484c7455c5f9ec370a62c311edcc73889bf315
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-10-07 22:01:26 -0500

    Refactor pom.xml and CustomerController
    - Update pom.xml to include mysql-connector-java dependency
    - Add new endpoints test4 and test5 to CustomerController
    - Create Customer entity class

--- src/main/java/ortega/clients/controllers/CustomerController.java
+++ src/main/java/ortega/clients/controllers/CustomerController.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ortega.clients.entities.Customer;
+
 @RestController // esto es una anotación
 public class CustomerController {
 
@@ -36,5 +38,27 @@ public class CustomerController {
     map.put("email", "Ivan_97@hotmail.com");
 
     return map;
+  }
+
+  @GetMapping("/test4")  
+  public Customer test4(){
+    Customer c = new Customer();
+    c.setFirstName("Ivan");
+    c.setLastName("Ortega");
+    c.setEmail("test@gmail.com");
+    c.setAddress("Calle falsa 123");
+
+    return c;
+  }
+
+  @GetMapping("/test5")
+  public List<Customer> test5(){
+    List<Customer> list = new ArrayList<>();
+    list.add(new Customer("Ivan", "Ortega", "test1@gmail.com", "Calle falsa 123"));
+    list.add(new Customer("Juan", "Perez", "test2@gmail.com", "Calle falsa 456"));
+    list.add(new Customer("Pedro", "Lopez", "test4@gmail.com", "Calle falsa 789"));
+    list.add(new Customer("Maria", "Gonzalez", "test5@gmail.com", "Calle falsa 101112"));
+
+    return list;
   }
 }
