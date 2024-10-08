130c6ddb52ff1e8a49067d4ad6b4283f8b8129e3
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-10-07 21:46:58 -0500

    Refactor CustomerController to add new endpoints for test2 and test3

--- src/main/java/ortega/clients/controllers/CustomerController.java
+++ src/main/java/ortega/clients/controllers/CustomerController.java
@@ -1,5 +1,10 @@
 package ortega.clients.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +14,27 @@ public class CustomerController {
   @GetMapping("/test")
   public String test(){
     return "Hello World";
+  }
+
+  @GetMapping("/test2")
+  public List<String> test2(){
+    List<String> list = new ArrayList<>();
+    list.add("Ivan");
+    list.add("Ortega");
+    list.add("Gonzalez");
+    list.add("Developer");
+
+    return list;
+  }
+
+  @GetMapping("/test3")
+  public Map<String, String> test3(){
+    Map<String, String> map = new HashMap<>();
+    map.put("name", "Ivan");
+    map.put("lastname", "Ortega");
+    map.put("nickname", "IvanOrtega");
+    map.put("email", "Ivan_97@hotmail.com");
+
+    return map;
   }
 }
