fa22bab3fea4e1ea36a078c39e1b22c49605efd1
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-05-23 19:20:20 -0500

    delete all

--- src/main/java/com/codewithmosh/store/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codewithmosh.store.repositories;
-
-import com.codewithmosh.store.entities.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
