3b43ba9c8ed876e8b91b8af9d94448a55831a886
llOrtegall <IvanOrtega_97@hotmail.com>
2025-06-17 14:53:26 -0500

    add methods to Produc Repository

--- supermarket/src/main/java/com/supermarket/persistence/ProductRepository.java
+++ supermarket/src/main/java/com/supermarket/persistence/ProductRepository.java
@@ -1,14 +1,38 @@
 package com.supermarket.persistence;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
 
 import com.supermarket.persistence.crud.ProductoCrudRepository;
 import com.supermarket.persistence.entity.Producto;
 
+@Repository
 public class ProductRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategory(int categoryId) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+    }
+
+    public Optional<List<Producto>> getLowStockProducts(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProduct(int productId) {
+        return productoCrudRepository.findById(productId);
+    }
+
+    public Producto saveProduct(Producto product) {
+        return productoCrudRepository.save(product);
+    }
+
+    public void deleteProduct(int productId) {
+        productoCrudRepository.deleteById(productId);
     }
 }
--- supermarket/src/main/java/com/supermarket/persistence/crud/ProductoCrudRepository.java
+++ supermarket/src/main/java/com/supermarket/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,17 @@
 package com.supermarket.persistence.crud;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import com.supermarket.persistence.entity.Producto;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    // @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery
+    // = true)
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(Integer idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
