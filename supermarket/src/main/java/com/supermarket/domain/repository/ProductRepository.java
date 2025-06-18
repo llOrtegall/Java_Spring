2f030ef931318cfcc6bd42dd18f6b06dad4bdb17
llOrtegall <IvanOrtega_97@hotmail.com>
2025-06-18 09:34:10 -0500

    add ProductRepository interface for product data access

---
+++ supermarket/src/main/java/com/supermarket/domain/repository/ProductRepository.java
@@ -0,0 +1,21 @@
+package com.supermarket.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.supermarket.domain.Product;
+
+public interface ProductRepository {
+
+    List<Product> getAll();
+
+    List<Product> getByCategory(int categoryId);
+
+    Optional<List<Product>> getScarseProducts(int quantity);
+
+    Optional<Product> getProduct(int productId);
+
+    Product save(Product product);
+
+    void delete(int productId);
+}
