38d98a692985078b1a0d6e89ea8cc1a6688e6a33
llOrtegall <IvanOrtega_97@hotmail.com>
2025-09-18 11:36:51 -0500

    feat: add CrudMovieEntity interface for movie entity persistence

---
+++ movies/src/main/java/play/movies/persistence/crud/CrudMovieEntity.java
@@ -0,0 +1,8 @@
+package play.movies.persistence.crud;
+
+import org.springframework.data.repository.CrudRepository;
+import play.movies.persistence.entity.MovieEntity;
+
+public interface CrudMovieEntity extends CrudRepository<MovieEntity, Long> {
+
+}
