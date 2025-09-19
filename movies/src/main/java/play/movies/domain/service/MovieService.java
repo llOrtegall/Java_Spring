b53d84942309ef78e06c1875e97cbb6d14c82302
llOrtegall <IvanOrtega_97@hotmail.com>
2025-09-19 17:06:08 -0500

    feat: implement MovieService for movie data retrieval

---
+++ movies/src/main/java/play/movies/domain/service/MovieService.java
@@ -0,0 +1,22 @@
+package play.movies.domain.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import play.movies.domain.dto.MovieDto;
+import play.movies.domain.repository.MovieRepository;
+
+@Service
+public class MovieService {
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public List<MovieDto> getAllMovies() {
+        return this.movieRepository.getAll();
+    }
+
+}
