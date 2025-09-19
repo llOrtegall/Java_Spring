c915ffa6bbb5a4be34cec085fecf860d0587c402
llOrtegall <IvanOrtega_97@hotmail.com>
2025-09-19 17:06:11 -0500

    feat: add MovieEntityRepository for movie data persistence

---
+++ movies/src/main/java/play/movies/persistence/MovieEntityRepository.java
@@ -0,0 +1,27 @@
+package play.movies.persistence;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import play.movies.domain.dto.MovieDto;
+import play.movies.domain.repository.MovieRepository;
+import play.movies.persistence.crud.CrudMovieEntity;
+import play.movies.persistence.mapper.MovieMapper;
+
+@Repository
+public class MovieEntityRepository implements MovieRepository {
+    private final CrudMovieEntity crudMovieEntity;
+    private final MovieMapper movieMapper;
+
+    public MovieEntityRepository(CrudMovieEntity crudMovieEntity, MovieMapper movieMapper){
+        this.crudMovieEntity = crudMovieEntity;
+        this.movieMapper = movieMapper;
+    }
+
+    @Override
+    public List<MovieDto> getAll() {
+        return this.movieMapper.toDto(this.crudMovieEntity.findAll());
+    }
+
+}
