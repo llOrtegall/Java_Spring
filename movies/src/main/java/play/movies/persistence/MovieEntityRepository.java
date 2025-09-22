856150fdd1f255af1529a188a7d0a3d85b1c11b1
llOrtegall <IvanOrtega_97@hotmail.com>
2025-09-22 07:46:29 -0500

    feat: add getById method in MovieEntityRepository for retrieving a movie by ID

--- movies/src/main/java/play/movies/persistence/MovieEntityRepository.java
+++ movies/src/main/java/play/movies/persistence/MovieEntityRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import play.movies.domain.dto.MovieDto;
 import play.movies.domain.repository.MovieRepository;
 import play.movies.persistence.crud.CrudMovieEntity;
+import play.movies.persistence.entity.MovieEntity;
 import play.movies.persistence.mapper.MovieMapper;
 
 @Repository
@@ -22,6 +23,12 @@ public class MovieEntityRepository implements MovieRepository {
     @Override
     public List<MovieDto> getAll() {
         return this.movieMapper.toDto(this.crudMovieEntity.findAll());
+    }
+
+    @Override
+    public MovieDto getById(long id) {
+        MovieEntity movieEntity = this.crudMovieEntity.findById(id).orElse(null);
+        return this.movieMapper.toDto(movieEntity);
     }
 
 }
