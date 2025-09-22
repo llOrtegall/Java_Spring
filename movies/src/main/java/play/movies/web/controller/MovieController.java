e358bd19f1d8d50a4fd7c2a497a1671cbe59bcab
llOrtegall <IvanOrtega_97@hotmail.com>
2025-09-22 07:45:52 -0500

    refactor: add new Get movie by id using PathVariable

--- movies/src/main/java/play/movies/web/controller/MovieController.java
+++ movies/src/main/java/play/movies/web/controller/MovieController.java
@@ -3,12 +3,15 @@ package play.movies.web.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import play.movies.domain.dto.MovieDto;
 import play.movies.domain.service.MovieService;
 
 @RestController
+@RequestMapping("/movies")
 public class MovieController {
     private final MovieService movieService;
 
@@ -16,9 +19,14 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @GetMapping("/movies")
+    @GetMapping
     public List<MovieDto> getAllMovies(){
         return this.movieService.getAllMovies();
+    }
+
+    @GetMapping("/{id}")
+    public MovieDto getById(@PathVariable long id){
+        return this.movieService.getById(id);
     }
 
 }
