181155501c34839e391688e00f2e6b988a91db07
llOrtegall <IvanOrtega_97@hotmail.com>
2025-09-23 09:35:20 -0500

    feat: add update and delete methods to MovieRepository interface

--- movies/src/main/java/play/movies/domain/repository/MovieRepository.java
+++ movies/src/main/java/play/movies/domain/repository/MovieRepository.java
@@ -3,9 +3,12 @@ package play.movies.domain.repository;
 import java.util.List;
 
 import play.movies.domain.dto.MovieDto;
+import play.movies.domain.dto.UpdateMovieDto;
 
 public interface MovieRepository {
     List<MovieDto> getAll();
     MovieDto getById(long id);
     MovieDto save(MovieDto movieDto);
+    MovieDto update(long id, UpdateMovieDto updateMovieDto);
+    void delete(long id);
 }
