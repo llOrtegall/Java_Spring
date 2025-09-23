ab350e0f8da3ae210329e4d6e6154cbb8f3509aa
llOrtegall <IvanOrtega_97@hotmail.com>
2025-09-23 09:35:36 -0500

    feat: update MovieController to return ResponseEntity for all endpoints and handle null cases

--- movies/src/main/java/play/movies/web/controller/MovieController.java
+++ movies/src/main/java/play/movies/web/controller/MovieController.java
@@ -2,12 +2,19 @@ package play.movies.web.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import play.movies.domain.dto.MovieDto;
+import play.movies.domain.dto.UpdateMovieDto;
 import play.movies.domain.service.MovieService;
 
 @RestController
@@ -20,13 +27,35 @@ public class MovieController {
     }
 
     @GetMapping
-    public List<MovieDto> getAllMovies(){
-        return this.movieService.getAllMovies();
+    public ResponseEntity<List<MovieDto>> getAllMovies(){
+        return ResponseEntity.ok(this.movieService.getAllMovies());
     }
 
     @GetMapping("/{id}")
-    public MovieDto getById(@PathVariable long id){
-        return this.movieService.getById(id);
+    public ResponseEntity<MovieDto> getById(@PathVariable long id){
+        MovieDto movieDto = this.movieService.getById(id);
+
+        if(movieDto == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(movieDto);
     }
 
-}
+    @PostMapping
+    public ResponseEntity<MovieDto> addNewMovie(@RequestBody MovieDto movieDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.movieService.addNewMovie(movieDto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MovieDto> updateMovie(@PathVariable long id, @RequestBody UpdateMovieDto updateMovieDto){
+        return ResponseEntity.ok(this.movieService.updateMovie(id, updateMovieDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteMovie(@PathVariable long id){
+        this.movieService.deleteMovie(id);
+        return ResponseEntity.ok("Movie Whit ID " + id + " was deleted sucefully");
+    }
+
+} 
