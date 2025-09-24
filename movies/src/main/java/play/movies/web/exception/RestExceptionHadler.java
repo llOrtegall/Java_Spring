ef7f016d0d25397297a6d2ca72b330c0f18c45c4
llOrtegall <IvanOrtega_97@hotmail.com>
2025-09-24 11:08:12 -0500

    feat: add new exception on valid data request handler

--- movies/src/main/java/play/movies/web/exception/RestExceptionHadler.java
+++ movies/src/main/java/play/movies/web/exception/RestExceptionHadler.java
@@ -1,6 +1,10 @@
 package play.movies.web.exception;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +17,23 @@ public class RestExceptionHadler {
     public ResponseEntity<Error> handleException(MovieAlreadyExistsException ex){
         Error error = new Error("movie already exist", ex.getMessage());
         return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<Error>> handleException(MethodArgumentNotValidException ex){
+        List<Error> errors = new ArrayList<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.add(new Error(error.getField(), error.getDefaultMessage()));
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Error> handleException(Exception ex){
+        Error error = new Error("uknow Error", ex.getMessage());
+        ex.printStackTrace();
+        return ResponseEntity.internalServerError().body(error);
     }
 }
